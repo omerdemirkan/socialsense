3d1aa56e87fde0f38c0629e97f144e08256e78bb
Emmad Usmani <emmadausmani@gmail.com>
2020-03-28 06:16:25 -0700

    started flask server

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+#TODO: change to post request
+#TODO: implement image ranking
+
+@app.route('/rank_images', methods=['GET'])
+def rank_images():
+    # sending JSON of dummy scores
+    res_body = {
+        'scores': [325, 1254, 90, 258, 721]
+    }
+    res = make_response(jsonify(res_body))
+    return res
+
+#TODO: change to post request
+#TODO: implement hashtag ranking
+
+@app.route('/rank_hashtags', methods=['GET'])
+def rank_hashtags():
+    # sending JSON of dummy hashtag, score pairs
+    res_body = {
+        'hashtags': [
+            {'cats': 0.09},
+            {'dogs': 0.05},
+            {'ootd': 0.13},
+            {'anime': 0.11}
+        ]
+    }
+    res = make_response(jsonify(res_body))
+    return res
