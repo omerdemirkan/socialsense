cb114aa1053d2431a72ea010277f79283ff95762
Omer Demirkan <omerfarukpiano@gmail.com>
2020-03-28 20:14:55 -0700

    resolve merge conflict

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,19 +4,10 @@ import classes from './Home.module.css';
 
 
 function Home() {
-    return (
-<<<<<<< HEAD
-    <div className = "bruh">
-        <h1 className = "big-text">Discover Your Audience</h1>
-        <p>
-            
-        </p>
-=======
-    <div className = {classes.Bruh}>
-        <h1 className>Discover your audience</h1>
->>>>>>> dc84634ca98537dc2b12439ee7bf6a11c65ac37e
+    return <div className = {classes.Bruh}>
+        <h1>Discover your audience</h1>
     </div>  
-    )
+    
 }
 
 export default Home
