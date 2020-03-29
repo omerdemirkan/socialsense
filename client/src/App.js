40a01d244b818d08fb220e20c2d1b72dc8cb732c
Taha Demirkan <tdemirkan2002@gmail.com>
2020-03-28 20:04:14 -0700

    'our mission' section

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 
+
 // Router
 import { Route, Switch } from 'react-router-dom';
 
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.module.css';
 
+
+
 function Home() {
     return (
     <div className = "bruh">
-        <h1 className = "big-text">Discover your audience</h1>
+        <h1 className = "big-text">Discover Your Audience</h1>
         <p>
             
         </p>
