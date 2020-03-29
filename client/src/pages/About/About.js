db01871e644ff1cbe1dfa3cd202f117f73f26705
Taha Demirkan <tdemirkan2002@gmail.com>
2020-03-28 22:05:30 -0700

    fixed about page

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './About.module.css';
+import classes from './About.module.css';
 
 export default function About() {
-    return <div>
+    return <div className = {classes.TextBox}>
         <h1>The Socialsense Mission</h1>
-        <p style = {{fontSize:"6vw", margin: "25px", color:"var(--medium)", textAlign: "center"}}>At Socialsense, we believe there is an easier way to grow your company by harnessing the power of hashtags. A more valuable, surefire way where more of your audience will customers. We're passionate about it, and our mission is to help people achieve the audience they deserve, and our mission starts with...</p>
+        <p style = {{fontSize:"6vw", margin: "25px", color:"var(--medium)", textAlign: "center"}}>At Socialsense, we believe there's an easier way to grow your company by harnessing the power of hashtags. A more valuable, surefire way where more of your audience will customers. We're passionate about it, and our mission is to help people achieve the audience they deserve, and our mission starts with...</p>
         <p style = {{fontSize:"8vw", margin: "25px", color:"var(--accent)", textAlign: "center"}}>#you</p>
     </div>
 }
